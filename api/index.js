cd5d625b2035d41ba091b5562c1cffeb098304d1
שמעון בוזגלו <shirizadok1@gmail.com>
2023-01-25 14:35:04 +0200

    auth v1 added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"; //when importing files always add the .js otherwise it will crash, for libareries no need
 
 const app = express();
 dotenv.config();
@@ -18,6 +19,9 @@ const connect = async () => {
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected!");
 });
+
+//middlewares
+app.use("/auth", authRoute);  //if you're using this endpoint, use authRoute component
 
 app.listen(3000, () => {
   connect();
