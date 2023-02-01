1d48d22bd456a9a626894ef932581b58cbe975d9
שמעון בוזגלו <shirizadok1@gmail.com>
2023-02-01 13:48:22 +0200

    user schema was added

---
+++ api/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique:true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique:true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model("User", UserSchema)
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,8 @@
 import express from "express";
+import { register } from "../controllers/auth";
 
 const router = express.Router();
 
-router.get("/", (req,res)=>{
-    res.send("Hello, this is auth endpoint")
-})
-router.get("/register", (req,res)=>{
-    res.send("Hello, this is auth register endpoint")
-})
+router.get("/register", register);
 
-export default router
+export default router;
